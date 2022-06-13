037cdcc137183b1c74380e71647efcd3b889f43d
cdt96 <cody.theroux3@gmail.com>
2022-06-13 16:24:50 -0400

    added my prompts for the user

--- index.js
+++ index.js
@@ -3,7 +3,44 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateReadMe = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Write the title of your project here.",
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message:"Write a description for your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Desribe the steps to take to install your project.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "List instructions for using your program.",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "List the collaborators on this project.",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "List your github username.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email.",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
