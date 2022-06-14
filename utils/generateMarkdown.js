5147d2c5ae42d6a09d89f5d5987e99a9907964b6
cdt96 <cody.theroux3@gmail.com>
2022-06-14 14:27:42 -0400

    changed the wording on line 36 on my generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,7 +33,7 @@ const generateReadMe = (data) => {
   readmetext += `\n## Installation\n${data.installation}\n`;
   readmetext += `\n## Usage\n${data.usage}\n`;
   readmetext += `\n## Contributors\n${data.contributors}\n`;
-  readmetext += `\n## License\n The license chosen to be used is ${data.licenses}\n`
+  readmetext += `\n## License\n This project is covered under the ${data.licenses} license.\n`
   readmetext += `\n## Questions? Please Contact Me\nGithub:https://github.com/${data.github}\n`;
   readmetext += `\nEmail:${data.email}\n`;
   return readmetext;
